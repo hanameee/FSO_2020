4fe52871651d07d1073cd2c2c28e6e9c5f7ef96c
hanameee <hanameee@gmail.com>
2020-11-09 22:41:17 +0900

    Feat: Modify courseData into JS array
    - Fix ts error (Object.values)
    - Modify courseData into one single JS array

--- frontend/src/components/Content.tsx
+++ frontend/src/components/Content.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Part from "./Part";
+import { ICourse } from "../index";
 
-const Content: React.FC<{ courses: { [key: string]: number } }> = ({
-    courses,
-}) => (
+const Content: React.FC<{ courseData: ICourse }> = ({ courseData }) => (
     <div>
-        {Object.entries(courses).map(([courseTitle, courseNumber]) => (
-            <Part
-                key={courseTitle}
-                courseTitle={courseTitle}
-                courseNumber={courseNumber}
-            />
+        {courseData.parts.map(({ name, exercises }) => (
+            <Part key={name} courseTitle={name} courseNumber={exercises} />
         ))}
     </div>
 );
