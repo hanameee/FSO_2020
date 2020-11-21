c5adcf35389748c55f9419eed14c42b6bad041e3
hanameee <hanameee@gmail.com>
2020-11-21 21:18:22 +0900

    Feat: Add delete, post API
    - Add DELETE /api/persons/:id
    - Add POST /api/persons
    - Add error logic for missing name, number, duplicate name

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 let persons = [
     { id: 1, name: "Hannah", number: "100" },
@@ -18,6 +21,37 @@ app.get("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id);
     const person = persons.find((person) => person.id === id);
     person ? res.json(person) : res.status(404).end();
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter((person) => person.id !== id);
+    res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+    const generateId = () => Math.floor(Math.random() * 10000);
+    const body = req.body;
+    if (!body.name) {
+        return res.status(400).json({
+            error: "name field missing",
+        });
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: "number field missing",
+        });
+    } else if (persons.find((person) => person.name === body.name)) {
+        return res.status(400).json({
+            error: "name must be unique",
+        });
+    }
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    };
+    persons = persons.concat(newPerson);
+    res.json(newPerson);
 });
 
 app.get("/info", (req, res) => {
